f9d52124f597ad0e1449670f5c0b87b0374a32b4
Richson-Tech <folorunshoahmedolajide@gmail.com>
2025-01-06 02:48:30 +0100

    Refactor code to improve performance and readability

---
+++ src/components/Involved.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Involved = () => {
+  return (
+    <section className="flex flex-col lg:flex-row items-center justify-center mt-5">
+      <div className="p-5">
+        <p className="text-[#f3a529] text-2xl font-semibold p-3 text-center">
+          GET INVOLVED
+        </p>
+        <h3 className="text-2xl lg:text-4xl font-semibold pt-3">
+          LET'S MAKE A DIFFERENCE TODAY
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default Involved;
