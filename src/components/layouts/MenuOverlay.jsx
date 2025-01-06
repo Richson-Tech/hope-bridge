a15485e74611b3607e751a133856d9f03168ae8e
Richson-Tech <folorunshoahmedolajide@gmail.com>
2025-01-06 01:01:42 +0100

    commit

--- src/components/layouts/MenuOverlay.jsx
+++ src/components/layouts/MenuOverlay.jsx
@@ -3,7 +3,7 @@ import NavLink from "./NavLink";
 
 const MenuOverlay = ({ links, closeMenu }) => {
   return (
-    <ul className="flex flex-col py-4 items-center">
+    <ul className="flex flex-col py-4 items-center bg-[#f3a529] text-white">
       {links.map((link, index) => (
         <li key={index} onClick={closeMenu}>
           <NavLink href={link.path} title={link.title} />
