abf0e1201b93d098b279379e2a95c1e0f07b5465
Richson-Tech <folorunshoahmedolajide@gmail.com>
2025-01-19 01:29:22 +0100

    Add CampaignSection component to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,16 @@ import HeroSection from "@/components/HeroSection";
 import Help from "@/components/Help";
 import CardsSection from "@/components/CardsSection";
 import AboutUsSection from "@/components/About";
+import CampaignSection from "@/components/CampaingSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <Help />
-          <CardsSection />
-          <AboutUsSection/>
+      <CardsSection />
+      <AboutUsSection />
+      <CampaignSection />
     </main>
   );
 }
