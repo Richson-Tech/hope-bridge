1ac9307d76a96c9ec47ffa27c8dbce7bd942663a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2025-02-21 23:58:07 +0100

    added new icons

--- src/components/layouts/MenuOverlay.jsx
+++ src/components/layouts/MenuOverlay.jsx
@@ -5,11 +5,11 @@ const MenuOverlay = ({ links, closeMenu }) => {
   return (
     // <ul className="flex flex-col py-4 items-center bg-[#f3a529] text-white">
     <ul className="flex flex-col fixed top-0 left-0 right-0 bottom-0 h-full pb-10 pt-4 overflow-y-auto bg-white">
-        {links.map((link, index) => (
-          <li key={index} onClick={closeMenu}>
-            <NavLink href={link.path} title={link.title} />
-          </li>
-        ))}
+      {links.map((link, index) => (
+        <li key={index} onClick={closeMenu}>
+          <NavLink href={link.path} title={link.title} />
+        </li>
+      ))}
     </ul>
   );
 };
--- src/components/layouts/NavLink.jsx
+++ src/components/layouts/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, title }) => {
   return (
     <Link
       href={href}
-      className="block py-2 pl-3 pr-4 text-white text-xl font-semibold rounded md:p-0 hover:text-white"
+      className="block py-2 pl-3 pr-4 text-black text-xl font-semibold rounded md:p-0 hover:text-black"
     >
       {title}
     </Link>
