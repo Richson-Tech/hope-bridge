df11bf0692c25d2fbb8fcc13d8af9b6d722aea3c
Richson-Tech <folorunshoahmedolajide@gmail.com>
2025-02-22 23:56:33 +0100

    Refactor import paths and update component structure

--- src/components/layouts/NavLink.jsx
+++ src/components/layouts/NavLink.jsx
@@ -1,26 +1,25 @@
 import Link from "next/link";
-import FacebookIcon from "@/assets/icons/facebook-circle.svg";
-import TwitterXIcon from "@/assets/icons/twitter-x.svg";
-import LinkedinIcon from "@/assets/icons/linkedin-fill.svg";
-import YoutubeIcon from "@/assets/icons/youtube-fill.svg";
-import InstagramIcon from "@/assets/icons/instagram-fill.svg";
+import FacebookIcon from "@/icons/facebook-circle.svg";
+import TwitterXIcon from "@/icons/twitter-x.svg";
+import YoutubeIcon from "@/icons/youtube-fill.svg";
+import InstagramIcon from "@/icons/instagram-fill.svg";
 
 const NavLink = ({ href, title }) => {
   return (
-    <Link
-      href={href}
-      className="block py-2 pl-3 pr-4 text-black text-xl font-semibold rounded md:p-0 hover:text-black"
-    >
-          {title}
-          
-    </Link>
-    <div className="flex gap-6 text-xl justify-center items-center py-2 text-brand-secondary">
-    <FacebookIcon />
-    <TwitterXIcon />
-    <LinkedinIcon />
-    <YoutubeIcon />
-    <InstagramIcon />
-  </div>
+    <>
+      <Link
+        href={href}
+        className="block py-2 pl-3 pr-4 text-black text-xl font-semibold rounded md:p-0 hover:text-black"
+      >
+        {title}
+      </Link>
+      <div className="flex gap-6 text-xl justify-center items-center py-2 text-brand-secondary">
+        <FacebookIcon />
+        <TwitterXIcon />
+        <YoutubeIcon />
+        <InstagramIcon />
+      </div>
+    </>
   );
 };
 
